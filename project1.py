df0dfa29160a0efb947350ba2064b91e6c144a31
Anika232323 <anikamb2303@gmail.com>
2025-04-18 13:05:26 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,17 @@
+from flask import Flask,request,render_template
+app=Flask(__name__)
+@app.route("/",methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        data={
+        "name":request.form.get("name"),
+        "roll":request.form.get("roll"),
+        "email":request.form.get("email"),
+        "phno":request.form.get("phno"),
+        "dept":request.form.get("dept"),
+        "event":request.form.get("event"),
+        }
+        return render_template("formresult.html",data=data)
+    return render_template("event registrations.html")
+if __name__=='__main__':
+    app.run(debug=True)
